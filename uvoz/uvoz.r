c5e4526aa770c0d4fdb6ea7b36f763886db1e78f
Geckaa <rok.zadravec@gmail.com>
2014-11-30 21:55:49 +0100

    Update uvoz.r

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -3,7 +3,6 @@
 # Funkcija, ki uvozi podatke iz datoteke ZDA.csv
 uvoziDrzave <- function() {
   return(read.table("podatki/ZDA.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
                       col.names = c("State", "Capital city", "Largest city", "Population", 
                                     "Total area", "Land area", "Percentage of population", "Size"),
                       fileEncoding = "Windows-1250"))
