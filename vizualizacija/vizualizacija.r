ac811b0055dbcb034f114b8584c74eb592d3b626
Geckaa <rok.zadravec@gmail.com>
2014-12-17 18:57:55 +0100

    Update vizualizacija.r

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,46 +1,25 @@
 # 3. faza: Izdelava zemljevida
 
-# Uvozimo funkcijo za pobiranje in uvoz zemljevida.
-source("lib/uvozi.zemljevid.r")
+# Najprej je treba naloziti paketa za delo z zemljevidi.
+library(sp)
+library(maptools)
+gpclibPermit()
 
-# Uvozimo zemljevid.
-cat("Uvažam zemljevid...\n")
-obcine <- uvozi.zemljevid("http://e-prostor.gov.si/fileadmin/BREZPLACNI_POD/RPE/OB.zip",
-                          "obcine", "OB/OB.shp", mapa = "zemljevid",
-                          encoding = "Windows-1250")
+#Narišemo zemljevid (zaradi preglednosti sem izpustil zvezno državo Alasko)
+pdf("slike/drzave_zda.pdf", width=6, height=4)
 
-# Funkcija, ki podatke preuredi glede na vrstni red v zemljevidu
-preuredi <- function(podatki, zemljevid) {
-  nove.obcine <- c()
-  manjkajo <- ! nove.obcine %in% rownames(podatki)
-  M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
-  names(M) <- names(podatki)
-  row.names(M) <- nove.obcine[manjkajo]
-  podatki <- rbind(podatki, M)
-  
-  out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$OB_UIME)[rank(zemljevid$OB_UIME)]), ]
-  if (ncol(podatki) == 1) {
-    out <- data.frame(out)
-    names(out) <- names(podatki)
-    rownames(out) <- rownames(podatki)
-  }
-  return(out)
-}
+USA <- readShapeLines("D:./././podatki/usa/statesp020.shp")
 
-# Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
-druzine <- preuredi(druzine, obcine)
+nocemo <- c("Alaska", "Hawaii", "Puerto Rico", "U.S. Virgin Islands")
+contiguous.states <- USA[!(USA$STATE %in% nocemo),]
+barve= if contiguous.states$AREA > 10, ["red"], ["green"]
+plot(contiguous.states, col=barve)
 
-# Izračunamo povprečno velikost družine.
-druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
-min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
-max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
+#Vrišemo glavna mesta na zemljevid
 
-# Narišimo zemljevid v PDF.
-cat("Rišem zemljevid...\n")
-pdf("slike/povprecna_druzina.pdf", width=6, height=4)
+text(coordinates(capitals[c("long", "lat")]), labels = capitals$capital,
+     pos = 1, cex = 0.6, offset = 0.3)
 
-n = 100
-barve = topo.colors(n)[1+(n-1)*(druzine$povprecje-min.povprecje)/(max.povprecje-min.povprecje)]
-plot(obcine, col = barve)
+
 
 dev.off()
