349ed7ccddeaaace06f78babde3c2f73fdb118b5
Geckaa <rok.zadravec@gmail.com>
2015-01-08 01:47:56 +0100

    Update uvoz.r

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,11 +1,14 @@
 # 2. faza: Uvoz podatkov
 
-# Funkcija, ki uvozi podatke iz datoteke ZDA.csv
-uvoziDrzave <- function() {
-  return(read.table("podatki/ZDA_fixed.csv", sep = ";", as.is = TRUE, skip = 1,
-                      col.names = c("State", "Capital city", "Largest city", "Population", 
-                                    "Total area", "Land area", "Percentage of population", "Size"),
-                      fileEncoding = "Windows-1250"))
-}
-cat("Uvažamo podatke o državah. \n")
-ZDA <- uvoziDrzave()
+# # Funkcija, ki uvozi podatke iz datoteke ZDA.csv
+# uvoziDrzave <- function() {
+#   return(read.table("podatki/ZDA_fixed.csv", sep = ";", as.is = TRUE, skip = 1,
+#                     col.names = c("State", "Capital city", "Largest city", "Population", 
+#                                   "Total area", "Land area", "Percentage of population", "Size"),
+#                     fileEncoding = "Windows-1250"))
+# }
+# cat("Uvažamo podatke o državah. \n")
+# ZDA <- uvoziDrzave()
+
+source("lib/xml.r")
+ZDA <- uvozi.drzave()
