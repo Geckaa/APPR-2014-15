a30b1ad22c96ec7521daa742b891d20dbcdfce01
Geckaa <rok.zadravec@gmail.com>
2015-01-14 01:37:18 +0100

    Update grafi.R

--- slike/grafi.R
+++ slike/grafi.R
@@ -1,11 +1,11 @@
-pdf("slike/grafi.pdf", paper="a4r")
+pdf("slike/grafi.pdf")
 
 #grafični prikaz populacije z imeni držav
 barplot(ZDA[1:6,c(5)]/1000000, names.arg = ZDA[1:6,c(1)], col = rainbow(50), xlab = "States", ylab = "Population x 1000000")
 
 dev.off()
 
-pdf("slike/grafi2.pdf", paper="a4r")
+pdf("slike/grafi2.pdf")
 
 #grafični prikaz populacije za vse države
 barplot(ZDA[1:51,c(5)]/1000000,horiz=TRUE, names.arg = ZDA[1:51,1], cex.names=0.7, col = rainbow(50), xlab = "Population x 1000000", ylab = "States", las=1)
