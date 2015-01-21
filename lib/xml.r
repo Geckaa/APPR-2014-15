b765a1b13267381c4d657dcb13c3ebb476ba4584
Geckaa <rok.zadravec@gmail.com>
2015-01-21 11:08:54 +0100

    Update xml.r

--- lib/xml.r
+++ lib/xml.r
@@ -1,14 +1,14 @@
 # Uvoz s spletne strani
- 
+
 library(XML)
- 
+
 # Vrne vektor nizov z odstranjenimi začetnimi "prazninami" (whitespace)
 # in iz vozlišč, ki ustrezajo podani poti.
 stripByPath <- function(x, path) {
   unlist(xpathApply(x, path,
                     function(y) gsub("^\\s*(.*?)\\s*$", "\\1",
-                                gsub("^(.*?)\\[.*$", "\\1",
-                                     xmlValue(y)))))
+                                     gsub("^(.*?)\\[.*$", "\\1",
+                                          xmlValue(y)))))
 }
 uvozi.drzave <- function() {
   url.drzave <- "http://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States"
@@ -34,17 +34,31 @@ uvozi.drzave <- function() {
   matrika <- matrix(unlist(seznam), nrow=length(seznam), byrow=TRUE)
   
   # Imena stolpcev matrike dobimo iz celic glave (<th>) v prvi vrstici
+  
   colnames(matrika) <- gsub("\n", " ", stripByPath(vrstice[[1]], ".//th")[-1])
+  
   # Imena vrstic matrike dobimo iz celic glave (<th>) v ostalih vrsticah
   rownames(matrika) <- unlist(lapply(vrstice[2:length(vrstice)], stripByPath, "./th"))
+  
+  # Znebimo se presledkov pred imeni držav.
+  rownames(matrika) <- gsub("[^A-Za-z ]", "",
+                            unlist(lapply(vrstice[2:length(vrstice)], stripByPath, "./th")))
+  
+  stripByPath <- function(x, path) {
+    unlist(xpathApply(x, path,
+                      function(y) gsub("[^[:alnum:], ]", "",
+                                       gsub("^\\s*(.*?)\\s*$", "\\1",
+                                            gsub("^(.*?)\\[.*$", "\\1",
+                                                 xmlValue(y))))))
+  }
   
   # Podatke iz matrike spravimo v razpredelnico
   # Prvi 4 stolpci: besedilo - pustimo, kot je
   # Zadnjih 5 stolpcev: številke - pobrišemo ločila tisočic in številke v oklepajih
   return(
     data.frame(matrika[,1:4], apply(gsub("([0-9]+).*", "\\1",
-                                    gsub(",", "", matrika[,5:9])),
-                                  2, as.numeric))
-    )
+                                         gsub(",", "", matrika[,5:9])),
+                                    2, as.numeric))
+  )
   
 }
