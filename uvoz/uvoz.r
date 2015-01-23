f9274012af5b7c6b0fc7bf428c2af0e58ef4d016
Geckaa <rok.zadravec@gmail.com>
2015-01-23 14:13:37 +0100

    Update uvoz.r

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -12,3 +12,29 @@
 
 source("lib/xml.r")
 ZDA <- uvozi.drzave()
+
+#dodamo stolpec ki državo uvrsti po velikosti na tri razrede
+attach(ZDA)
+moznosti <- c("Large", "Average", "Small")
+stolpec <- character(nrow(ZDA))
+stolpec[Total.area.in.mi2..km2. > 100000] <- "Large"
+stolpec[Total.area.in.mi2..km2. <100000 & Total.area.in.mi2..km2. > 50000] <- "Average"
+stolpec[Total.area.in.mi2..km2. < 50000 ] <- "Small"
+Size <- factor(stolpec, levels = moznosti, ordered = TRUE)
+detach(ZDA)
+ZDA <- data.frame(ZDA, Size)
+
+#dodal stolpec z gostoto prebivalstva
+ZDA["Pop.Density"] <- NA
+ZDA$Pop.Density <- ZDA$Population..2013.est. / ZDA$ Total.area.in.mi2..km2.
+
+#gostota prebivalstva (gledano samo na površino zemlje)
+ZDA["Pop.Density.per.Land.area"] <- NA
+ZDA$Pop.Density.per.Land.area <- ZDA$Population..2013.est. / ZDA$Land.area.in.mi2..km2.
+
+#kolikšen delež države je zemeljski
+ZDA["Percentage.of.Land.area"] <- NA
+ZDA$Percentage.of.Land.area <-ZDA$Land.area.in.mi2..km2. / ZDA$Total.area.in.mi2..km2.
+
+#tabela v kateri so države namesto po ebecedi razporejene po populaciji
+ZDA_po_pop. <- ZDA[order(ZDA$Population..2013.est., decreasing=TRUE),]
