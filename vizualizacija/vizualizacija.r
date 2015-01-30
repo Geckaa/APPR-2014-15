5857695b12675f9eb07840b9627810b6b5bb7fdf
Geckaa <rok.zadravec@gmail.com>
2015-01-30 14:19:18 +0100

    Update vizualizacija.r

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -11,6 +11,7 @@ pdf("slike/drzave_zda.pdf")
 
 # USA1 <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2/shp/USA_adm.zip",
 #                        "USA", "USA_adm1.shp", mapa = "zemljevidi")
+
 USA <- uvozi.zemljevid("http://audrey.fmf.uni-lj.si/states_21basic.zip",
                           "USA", "states.shp", mapa = "zemljevid")
 
@@ -18,21 +19,23 @@ nocemo <- c("Alaska", "Hawaii", "Puerto Rico", "U.S. Virgin Islands")
 states <- USA[!(USA$STATE_NAME %in% nocemo),]
 
 m <- match(states$STATE_NAME, rownames(ZDA))
-pop <- ZDA[m,5] # populacija je v 5. stolpcu
+pop <- ZDA[m,4] # populacija je v 4. stolpcu
 n <- 4 # 4 kategorije
 q <- quantile(pop, (1:n)/n, na.rm = TRUE)
 barve <- topo.colors(n)
-plot(states, col = barve[sapply(pop, function(x) which(x <= q)[1])])
+plot(states, border = "dimgray", col = barve[sapply(pop, function(x) which(x <= q)[1])])
 
 legend("bottomleft",
-       legend=c("Highest pop.", "High pop.", "Low pop.", "Lowest pop."), col=c("yellow","green","cyan","blue"),
+       legend=c("Največja populacija", "Visoka populacija", "Nizka populacija", "Najnižja populacija"), col=c("yellow","green","cyan","blue"),
        lty = c("solid", "solid", "solid", "solid"),
        lwd = c(10, 10, 10, 10),
        bg = "white",
        title="Legenda")
 
 #Vrišemo glavna mesta na zemljevid
+#Iz datoteke uscapitals.csv sem izvzel določena imena glavnih mest zaradi prenatrpanosti
 capitals <- read.csv("podatki//uscapitals_1.csv", row.names = 1)
+
 points(coordinates(capitals[c("long", "lat")]),
        col = "red",
        pch = 10, cex = 0.2)
