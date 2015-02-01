a0c60dea6774cfcd420475d497e3d4be33428d00
Geckaa <rok.zadravec@gmail.com>
2015-02-01 16:03:44 +0100

    Update analiza.r

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,17 +1,43 @@
 # 4. faza: Analiza podatkov
 
-# Uvozimo funkcijo za uvoz spletne strani.
-source("lib/xml.r")
+#Leto 1935 je naše izhodišče, zato ga postavimo kot leto 0
 
-# Preberemo spletno stran v razpredelnico.
-cat("Uvažam spletno stran...\n")
-tabela <- preuredi(uvozi.obcine(), obcine)
+pdf("slike/populacija.pdf")
 
-# Narišemo graf v datoteko PDF.
-cat("Rišem graf...\n")
-pdf("slike/naselja.pdf", width=6, height=4)
-plot(tabela[[1]], tabela[[4]],
-     main = "Število naselij glede na površino občine",
-     xlab = "Površina (km^2)",
-     ylab = "Št. naselij")
+plot(leto, pop, xlab = "Leto", ylab = "Populacija x 1000000")
+
+leto <- demographics$Year - 1900
+pop <- demographics$Population.x1000 / 1000
+
+#Npišemo funkcijo za linearno rast
+
+linearna <- lm(pop ~ leto)
+abline(linearna, col="blue")
+
+#Preverimo če je populacija kvadratna funkcija
+
+kvadratna <- lm(pop ~ I(leto^2) + leto)
+curve(predict(kvadratna, data.frame(leto=x)), add = TRUE, col = "red")   
+
+#Loess model za primerjavo (model loess uporablja lokalno prilagajanje)
+
+loess <- loess(pop ~ leto)
+
+#Pogledamo ostanke pri modelih. Tisti, ki ima manjši ostanek je bolj natančen
+ostanki <- sapply(list(linearna, kvadratna, loess, power3, power4, power9), function(x) sum(x$residuals^2))
+
+dev.off()
+
+#Narisali bomo napoved za rast prebivalstva do leta 2030 po modelu za modeala linear in kvadratna
+
+pdf("slike/napoved.pdf")
+
+plot(demographics$Year, pop, xlim = c(1935, 2050), ylim = c(120, 450), 
+     xlab = "Leto", ylab = "Populacija (v mil)")     
+
+napoved <- function(x,model){predict(model, data.frame(leto=x-1900))}
+
+curve(napoved(x, linearna), add= TRUE, col = "blue")
+curve(napoved(x, kvadratna), add = TRUE, col = "red")
+
 dev.off()
