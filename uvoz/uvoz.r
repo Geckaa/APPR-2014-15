676179aecac960d61b627e6b54806d95b0378b64
Geckaa <rok.zadravec@gmail.com>
2015-02-01 15:05:18 +0100

    Update uvoz.r

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -20,32 +20,34 @@ demographics <- demographics()
 source("lib/xml.r")
 ZDA <- uvozi.drzave()
 
-#dodamo stolpec ki državo uvrsti po velikosti na tri razrede
-attach(ZDA)
-moznosti <- c("Large", "Average", "Small")
-stolpec <- character(nrow(ZDA))
-stolpec[Total.area.in.mi2..km2. > 100000] <- "Large"
-stolpec[Total.area.in.mi2..km2. <100000 & Total.area.in.mi2..km2. > 50000] <- "Average"
-stolpec[Total.area.in.mi2..km2. < 50000 ] <- "Small"
-Size <- factor(stolpec, levels = moznosti, ordered = TRUE)
-detach(ZDA)
-ZDA <- data.frame(ZDA, Size)
-
+#Znebimo se stolpcev Statehood in pa House.seats.s ker me podatki ne zanimajo
 ZDA$Statehood <- NULL
 ZDA$House.seat.s. <- NULL
 
+colnames(ZDA) <- c("Abbr", "Capital", "Largest.City", "Population.2013", "Total.area.in.mi2", 
+                   "Land.area.in.mi2", "Water.area.in.mi2")
+
 #Tabeli ZDA dodal stolpec z gostoto prebivalstva
 ZDA["Pop.Density"] <- NA
-ZDA$Pop.Density <- ZDA$Population..2013.est. / ZDA$ Total.area.in.mi2..km2.
+ZDA$Pop.Density <- ZDA$Population.2013 / ZDA$ Total.area.in.mi2
 
 #Tabeli ZDA dodal stolpec z gostoto prebivalstva (gledano samo na površino zemlje)
 ZDA["Pop.Density.per.Land.area"] <- NA
-ZDA$Pop.Density.per.Land.area <- ZDA$Population..2013.est. / ZDA$Land.area.in.mi2..km2.
+ZDA$Pop.Density.per.Land.area <- ZDA$Population.2013 / ZDA$Land.area.in.mi2
 
 #Tabeli ZDA dodal stolpec s podatkom kolikšen delež države je kopnega
 ZDA["Percentage.of.Land.area"] <- NA
-ZDA$Percentage.of.Land.area <-ZDA$Land.area.in.mi2..km2. / ZDA$Total.area.in.mi2..km2.
+ZDA$Percentage.of.Land.area <-ZDA$Land.area.in.mi2 / ZDA$Total.area.in.mi2
+
+#Dodamo še stolpec ki državo uvrsti po velikosti na tri razrede
+moznosti <- c("Large", "Average", "Small")
+stolpec <- character(nrow(ZDA))
+stolpec[Total.area.in.mi2 > 100000] <- "Large"
+stolpec[Total.area.in.mi2 <100000 & Total.area.in.mi2 > 50000] <- "Average"
+stolpec[Total.area.in.mi2 < 50000 ] <- "Small"
+Size <- factor(stolpec, levels = moznosti, ordered = TRUE)
+ZDA <- data.frame(ZDA, Size)
 
 #Tabela  v kateri so države namesto po abecedi razporejene po populaciji
 #To sem naredil da sem narisal graf kjer so države razporejene po populaciji namesti abecedi
-ZDA_pop. <- ZDA[order(ZDA$Population..2013.est., decreasing=TRUE),]
+ZDA_pop <- ZDA[order(ZDA$Population.2013, decreasing=TRUE),]
