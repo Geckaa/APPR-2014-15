34abffcaaffa1f4dafbf5b4cfa7ecd75848a7744
Geckaa <rok.zadravec@gmail.com>
2015-02-08 14:56:59 +0100

    Update grafi.R

--- slike/grafi.R
+++ slike/grafi.R
@@ -2,7 +2,9 @@ pdf("slike/grafi.pdf")
 
 #grafični prikaz populacije z imeni držav
 
-barplot(ZDA[1:51, c(6)]/100000, horiz=TRUE, names.arg = ZDA[1:51,c(1)], cex.names=0.7, col = rainbow(50), xlab = "Area (Mi^2) x 100000", ylab = "States", las=1)
+barplot(ZDA[1:51, c(6)]/100000, horiz=TRUE, names.arg = ZDA[1:51,c(1)],
+        cex.names=0.7, col = rainbow(50), xlab = "Area (Mi^2) x 100000", ylab = "States", las=1,
+        main = "Graf velikosti")
 
 dev.off()
 
@@ -11,19 +13,21 @@ pdf("slike/grafi2.pdf")
 
 #grafični prikaz populacije za vse države
 
-barplot(ZDA_pop[1:51, c(5)]/1000000, horiz=TRUE, names.arg = ZDA_pop[1:51,1], cex.names=0.7, col = rainbow(50), xlab = "Population x 1000000", ylab = "States", las=1)
+barplot(ZDA_pop[1:51, c(4)]/1000000, horiz=TRUE, names.arg = ZDA_pop[1:51,1],
+        cex.names=0.7, col = rainbow(50), xlab = "Population x 1000000", ylab = "States", las=1,
+        main = "Graf prebivalstva")
 
 dev.off()
 
+leto <- demographics$Year
+pop <- demographics$Population.x1000
 
 pdf("slike/grafi3.pdf")
 
 #prikaz spremembe populacije po letih
 
-leto <- demographics$Year
-pop <- demographics$Population.x1000
-
 plot(leto, pop / 1000, 
-     xlab = "Year", ylab = "Population x1000000", type = "h", lwd = 3.2, col = "steelblue")
+     xlab = "Year", ylab = "Population x1000000", type = "h", lwd = 3.2, col = "steelblue",
+     main = "Graf predstavja prebivalstvo v celitnih ZDA")
 
 dev.off()
